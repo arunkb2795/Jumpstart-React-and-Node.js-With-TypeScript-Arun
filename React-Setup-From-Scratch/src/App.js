d07b698cd98a3a34049a13556130ec6072d49dce
arun8018 <arun.babu@litmus7.com>
2022-05-01 19:48:12 +0530

    completed

--- React-Setup-From-Scratch/src/App.js
+++ React-Setup-From-Scratch/src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
 export default function App() {
+  const Header = styled.h1`
+    color: red;
+    @media (max-width: 480px) {
+      font-size: 24px;
+      color: green;
+    }
+  `;
+
+  const Hero = styled(Header)`
+    color: blue;
+    font-size: 100px;
+  `;
+
   return (
-    <div>Welcome to React-Setup from Scratch</div>
-  )
+    <div>
+      <Header>Hello React!</Header>
+      <Hero>I am a Hero.</Hero>
+    </div>
+  );
 }
